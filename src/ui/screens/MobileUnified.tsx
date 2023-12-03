56470ba62c6801884b08bb1f521e6e16304641f1
Doctor Delpy <delpydoc@proton.me>
2023-12-03 06:39:55 +0000

    fix(mobile-nav): remove remaining settingsOpen usage in FAB visibility condition; rely on page==='home'

--- src/ui/screens/MobileUnified.tsx
+++ src/ui/screens/MobileUnified.tsx
@@ -293,7 +293,7 @@ export function MobileUnified({ methods, defaultMethod, autosaveQuick, blocked, 
       )}
 
       {/* Floating action button to open generate sheet */}
-      {!blocked && !settingsOpen && !pwModal.open && !consoleOpen && (
+      {!blocked && page === 'home' && !pwModal.open && !consoleOpen && (
         <button className="fab" aria-label={t('generate')} title={t('generate')} onClick={() => { setConsoleOpen(true); setConsoleStep('form'); setRevealed(false) }}>
           <svg width="20" height="20" viewBox="0 0 24 24" aria-hidden><path fill="currentColor" d="M12 5v14m-7-7h14" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/></svg>
         </button>
